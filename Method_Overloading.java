7088efdf3cec556237ad440de7792efb6708e81e
Neha Tanksale <34652815+nehatanksale@users.noreply.github.com>
2018-06-22 17:46:28 +0530

    Update and rename Method_Overloading to Method_Overloading.java

--- Method_Overloading.java
+++ Method_Overloading.java
@@ -2,7 +2,7 @@ public class Method_Overloading
 {
     public static void main(String[] args)
     {
-        binary obj=new binary();
+        Method_Overloading obj=new Method_Overloading();
         obj.show();
         obj.show(2,5);
     }
