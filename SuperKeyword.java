6acd7f237d833e59dd35589943df021c15652af3
Neha Tanksale <34652815+nehatanksale@users.noreply.github.com>
2018-06-24 11:46:01 +0530

    Create SuperKeyword.java

---
+++ SuperKeyword.java
@@ -0,0 +1,19 @@
+
+public class SuperKeyword {
+    int acc_no=10;
+
+    public static void main(String args[]) {
+
+    }
+}
+ class bin extends binary
+{
+    public bin()
+    {
+        System.out.println(super.acc_no);
+
+
+    }
+
+}
+
