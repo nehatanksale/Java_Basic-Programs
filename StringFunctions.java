1648d4f170d13ba9ee3880d2a51eabf5e8c834a9
Neha Tanksale <34652815+nehatanksale@users.noreply.github.com>
2018-06-28 12:54:59 +0530

    Create StringFunctions.java

---
+++ StringFunctions.java
@@ -0,0 +1,15 @@
+class StringFunctions
+{
+    public static void main(String[] args) {
+            String s="HelloWorld";
+        System.out.println( s.charAt(4));
+        System.out.println(s.concat("HelloHello"));
+        System.out.println(s.equals("Hello"));
+        System.out.println( s.getBytes());
+        System.out.println( s.length());
+        System.out.println( s.indexOf('h'));
+        System.out.println( s.trim());
+
+
+    }
+}
