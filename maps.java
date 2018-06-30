6e378efce70f12141ab06aab73d4f967fca8eda6
Neha Tanksale <34652815+nehatanksale@users.noreply.github.com>
2018-06-30 15:42:17 +0530

    Create maps.java

---
+++ maps.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class maps
+{
+        public static void main(String[] args) {
+
+            Map<String,String> languages=new HashMap<>();
+
+            ((HashMap) languages).put("Java","object oriented");
+            System.out.println(languages.get("Java"));
+
+
+
+
+        }
+}
