972de334b22a489aaf572e8a85544c9d4de7029d
Neha Tanksale <34652815+nehatanksale@users.noreply.github.com>
2018-06-30 19:46:38 +0530

    Create Set.java

---
+++ Set.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Set {
+
+
+
+
+    public static void main(String[] args) {
+
+        Set<String> set1=new HashSet<String>();
+        set1.add("element1");
+        set1.add("element2");
+
+        System.out.println( set1);
+    }
+}
